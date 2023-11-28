75fe92dc136022ed65cc24a01708c8f517878f26
Paul Beardwell <beardwell@hotmail.com>
2023-11-28 16:34:41 +0000

    Created a blank page to display lessons

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AddLesson
+from .views import AddLesson, LessonList
 
 urlpatterns = [
     path('', AddLesson.as_view(), name='add_lesson'),
+    path('lessons/', LessonList.as_view(), name='modules'),
 ]
