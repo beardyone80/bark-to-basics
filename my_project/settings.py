6e3a0c14270f39613d3c75b9b24ce6a0850db5f9
hibo ibrahim <hibo.ibrahim@gmail.com>
2023-11-29 09:58:22 +0000

    style footer and social icons

--- my_project/settings.py
+++ my_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-beardyone80-barktobasic-hdpr91krzgf.ws-eu106.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-beardyone80-barktobasic-kqvjc3lu376.ws-us106.gitpod.io','.herokuapp.com']
 
 
 # Application definition
